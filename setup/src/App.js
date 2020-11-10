4aac0dadd6a094bf8e85ce703923594d1277be9b
tabit <tabithakavyu4@gmail.com>
2020-11-11 01:28:05 +0300

    title edit

--- setup/src/App.js
+++ setup/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <section className='container'>
-        <h3>{people.length} birthdays today</h3>
+        <h3>{people.length} birthdays to Remember</h3>
         <List people={people} />
         <button onClick={()=> setPeople([])}>clear all</button>
       </section>
